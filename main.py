ffc1dd16c4339e335b79d30d7647366b9716b585
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-21 13:17:38 -0400

    refactor

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ filepaths = glob.glob("xlsx/*.xlsx")
 for file in filepaths:
 	# Gather the data
 	df = pd.read_excel(file, sheet_name="Sheet 1")
-	order_number = Path(file).stem.split("-")[0]
+	order_number, date = Path(file).stem.split("-")
 
 	# Create a new PDF
 	pdf = FPDF(orientation="portrait", unit="mm", format="a4")
@@ -17,7 +17,9 @@ for file in filepaths:
 
 	# Write the data
 	pdf.set_font(family="times", size= 24, style="B")
-	pdf.cell(w=0, h=8, txt=f"Order num. {order_number}" )
+	pdf.cell(w=0, h=15, ln=1, txt=f"Order number: {order_number}" )
+	pdf.set_font(family="times", size= 18, style="B")
+	pdf.cell(w=0, h=10, ln=1, txt=f"Date (y/m/d): {date}" )
 
 	# Save and export
 	pdf.output(f"PDFs/{order_number}-store_name.pdf")

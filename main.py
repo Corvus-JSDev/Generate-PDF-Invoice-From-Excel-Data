a7c9094c73ee116c85ed05e187558b6e3dbb1f42
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-20 20:15:40 -0400

    import pandas and update gitignore

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import fpdf
 import pandas as pd
 
+df1 = pd.read_excel("xlsx/10001-2023.1.18.xlsx")
+df2 = pd.read_excel("xlsx/10002-2023.1.18.xlsx")
+df3 = pd.read_excel("xlsx/10003-2023.1.18.xlsx")

bd30edf4091062f67a308b6e24535ef4ca75206e
GiovanniS <giovanni.squillace.work@gmail.com>
2024-08-20 20:29:14 -0400

    read and print the data

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import glob
 import fpdf
 import pandas as pd
 
-df1 = pd.read_excel("xlsx/10001-2023.1.18.xlsx")
-df2 = pd.read_excel("xlsx/10002-2023.1.18.xlsx")
-df3 = pd.read_excel("xlsx/10003-2023.1.18.xlsx")
+filepaths = glob.glob("xlsx/*.xlsx")
+# print(filepath)  # ['xlsx/10002-2023.1.18.xlsx', 'xlsx/10001-2023.1.18.xlsx', 'xlsx/10003-2023.1.18.xlsx']
+
+index = 1
+for file in filepaths:
+	df = pd.read_excel(file, sheet_name="Sheet 1")
+	print(f"------- Printing file {index} -------")
+	print(df)
+	print(" ")
+	index += 1
